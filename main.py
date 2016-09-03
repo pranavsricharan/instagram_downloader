8fb426b6b3cce44ad785309162e27810a5db62e6
pranavsricharan <pranav.sricharan@gmail.com>
2016-09-03 11:42:23 +0530

    Video
    Added support to download instagram videos

--- main.py
+++ main.py
@@ -13,18 +13,45 @@ while ch == 'y':
     data = urllib.request.urlopen(url)
     data = data.read()
     data = str(data)
-    regex = ".*?<meta property=\"og:image\" content=\"(.*?)\?.*?\" />.*?"
+    # Find video URL
+    regex = ".*?<meta property=\"og:video[^:]??\" content=\"(.*?)\".*?\" />"
     imageUrl = re.match(regex,data)
-    imageUrl = imageUrl.group(1)
-    print("Found image:",imageUrl)
-    image = urllib.request.urlopen(imageUrl)
-    image = image.read()
-    filename = re.match(".*?/([^/]*?\.jpg)$",imageUrl)
-    filename = filename.group(1)
-    file = open("img/"+filename,"wb")
-    file.write(image)
-    file.close()
-    print("File saved at:","img/"+filename,"of program directory")
+    # Check if any video exists
+    if(imageUrl is not None):
+        imageUrl = imageUrl.group(1)
+        print("Found video:",imageUrl)
+        # Open and get the contents of the video
+        image = urllib.request.urlopen(imageUrl)
+        image = image.read()
+        # Get the file name from the URL
+        filename = re.match(".*?/([^/]*?\.*?)$",imageUrl)
+        filename = filename.group(1)
+        # Save the video to the local file
+        file = open("img/"+filename,"wb")
+        file.write(image)
+        file.close()
+        print("File saved at:","img/"+filename,"of program directory")
+    else: # If the URL is not a video
+        #Find the image URL
+        regex = ".*?<meta property=\"og:image\" content=\"(.*?)\?.*?\" />.*?"
+        imageUrl = re.match(regex,data)
+        # Check if any image exists
+        if(imageUrl is not None):
+            imageUrl = imageUrl.group(1)
+            print("Found image:",imageUrl)
+            # Open URL and get image contents
+            image = urllib.request.urlopen(imageUrl)
+            image = image.read()
+            # Get the file name from the URL
+            filename = re.match(".*?/([^/]*?\.jpg)$",imageUrl)
+            filename = filename.group(1)
+            # Save the image to local file
+            file = open("img/"+filename,"wb")
+            file.write(image)
+            file.close()
+            print("File saved at:","img/"+filename,"of program directory")
+        else:
+            print("Some error occured...")
     ch = input("Another image? (y/n): ")
     ch = ch.lower()
 x = input("Press return to exit")
